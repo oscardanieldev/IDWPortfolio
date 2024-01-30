71b15660e680c355b2e86b1c2e76d2105ec07e56
oscardanieldev <o.solares.m@gmail.com>
2024-01-30 13:09:26 -0600

    Add CArd
    Agrega cartas de servicio de manera estática

--- src/Screens/Services/Services.jsx
+++ src/Screens/Services/Services.jsx
@@ -1,12 +1,13 @@
 import './Services.css';
 import {Grid, Box} from '@mui/material';
 import {useTranslation} from "react-i18next"
-import Card from '@mui/material/Card';
+// import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import CardHeader from '@mui/material/CardHeader';
 import salforcelogo  from './img/salforce-logo.png';
 import Typography from '@mui/material/Typography';
+import Card from 'react-bootstrap/Card';
 // import Rotate from 'react-reveal/Rotate';
 function Services() {
 
@@ -26,52 +27,66 @@ function Services() {
           <Grid container >
             <Grid item md={6} xs={12} className="">
                   <Box sx={{ flexGrow: 1, p: { xs: 2, md: 8 }, mb: {xs: 2, md: 4} }} className="sobreMiInfo">
-                    <Card sx={{ maxWidth: '100vw' }} >
-                      <CardHeader
-                       title="Consultoría y desarrollo Salesforce"
-                      />
-                        <CardMedia
-                          component="img"
-                          height="100"
-                          src={salforcelogo}
-                          alt="Salesforce Logo"
-                        />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                          Lizard
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          Lizards are a widespread group of squamate reptiles, with over 6,000
-                          species, ranging across all continents except Antarctica
-                        </Typography>
-                      </CardContent>
-               
-                    </Card>
+                    <Card style={{ width: '100%', backgroundColor: 'red'  }}>
+                        <Card.Body>
+                          <Card.Title>{t('Services.webDeveloper-title')}</Card.Title>
+                          <Card.Subtitle className="mb-2 text-muted">{t('Services.webDeveloper-title')}</Card.Subtitle>
+                          <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                          </Card.Text>
+                          <Card.Link href="#">Card Link</Card.Link>
+                          <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                      </Card>
                   </Box>  
             </Grid>
             <Grid item md={6} xs={12} className="">
                   <Box sx={{ flexGrow: 1, p: { xs: 2, md: 8 }, mb: {xs: 2, md: 4} }} className="sobreMiInfo">
-                    <Card sx={{ maxWidth: '100vw' }} >
-                      <CardHeader
-                       title="Consultoría y desarrollo Salesforce"
-                      />
-                        <CardMedia
-                          component="img"
-                          height="100"
-                          src={salforcelogo}
-                          alt="Salesforce Logo"
-                        />
-                        <CardContent>
-                        <Typography gutterBottom variant="h5" component="div">
-                          Lizard
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                          Lizards are a widespread group of squamate reptiles, with over 6,000
-                          species, ranging across all continents except Antarctica
-                        </Typography>
-                      </CardContent>
-               
-                    </Card>
+                    <Card style={{ width: '100%', backgroundColor: 'red'  }}>
+                        <Card.Body>
+                          <Card.Title>{t('Services.salesforce-title')}</Card.Title>
+                          <Card.Subtitle className="mb-2 text-muted">{t('Services.salesforce-title')}</Card.Subtitle>
+                          <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                          </Card.Text>
+                          <Card.Link href="#">Card Link</Card.Link>
+                          <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                      </Card>
+                  </Box>  
+            </Grid>
+            <Grid item md={6} xs={12} className="">
+                  <Box sx={{ flexGrow: 1, p: { xs: 2, md: 8 }, mb: {xs: 2, md: 4} }} className="sobreMiInfo">
+                    <Card style={{ width: '100%', backgroundColor: 'red'  }}>
+                        <Card.Body>
+                          <Card.Title>{t('Services.eCommerce-title')}</Card.Title>
+                          <Card.Subtitle className="mb-2 text-muted">{t('Services.eCommerce-title')}</Card.Subtitle>
+                          <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                          </Card.Text>
+                          <Card.Link href="#">Card Link</Card.Link>
+                          <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                      </Card>
+                  </Box>  
+            </Grid>
+            <Grid item md={6} xs={12} className="">
+                  <Box sx={{ flexGrow: 1, p: { xs: 2, md: 8 }, mb: {xs: 2, md: 4} }} className="sobreMiInfo">
+                    <Card style={{ width: '100%', backgroundColor: 'red'  }}>
+                        <Card.Body>
+                          <Card.Title>{t('Services.mobileDeveloper-title')}</Card.Title>
+                          <Card.Subtitle className="mb-2 text-muted">{t('Services.mobileDeveloper-title')}</Card.Subtitle>
+                          <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                          </Card.Text>
+                          <Card.Link href="#">Card Link</Card.Link>
+                          <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                      </Card>
                   </Box>  
             </Grid>
           </Grid>
